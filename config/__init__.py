69b91eea8ecbf285fed896b82555f8e4d7735461
eshwarandevpy <eshwarandevpy@gmail.com>
2025-01-19 03:45:04 -0800

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+# eshwarandevpy
+# Copyright (C) 2025 by eshwarandevpy@Github, < https://github.com/eshwarandevpy >.
+#
+# This file is part of < https://github.com/eshwarandevpy/eshwaranbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/eshwarandevpy/eshwaranbot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
